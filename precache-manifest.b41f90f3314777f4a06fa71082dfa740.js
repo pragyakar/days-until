82811045fa793cf6e61a5badc3105e7e39257c67
pragyakar <pragyakar@fusemachines.com>
2019-04-14 02:08:49 +0545

    Updates

---
+++ precache-manifest.b41f90f3314777f4a06fa71082dfa740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffd9720ab704100af4d6",
+    "url": "/days-until/static/css/main.07d63291.chunk.css"
+  },
+  {
+    "revision": "ffd9720ab704100af4d6",
+    "url": "/days-until/static/js/main.edf60913.chunk.js"
+  },
+  {
+    "revision": "371af37516f9151b9349",
+    "url": "/days-until/static/js/runtime~main.3ee62d4c.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/days-until/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1ac3842aeaad05aad39bbd05a3068d4f",
+    "url": "/days-until/index.html"
+  }
+];
