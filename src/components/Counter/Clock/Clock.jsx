41597dd3998176a93b2c08c3bb9a1617eb34830f
pragyakar <pragyakar@fusemachines.com>
2019-04-13 20:06:36 +0545

    :sparkles: Added clock component

---
+++ src/components/Counter/Clock/Clock.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0, 
+      minutes: 0,
+      seconds: 0
+    }
+  }
+
+  componentWillMount() {
+    this.timeDifference(this.props.nextHolidayDate);
+  }
+
+  componentDidMount() {
+    setInterval(() => (
+      this.timeDifference(this.props.nextHolidayDate)
+    ), 1000);
+  }
+  
+  timeDifference(nextDate) {
+    const timeDifference = nextDate - Date.parse(new Date()); 
+    this.setState({
+      days: Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor(timeDifference / (1000 * 60 * 60) % 24), 
+      minutes: Math.floor((timeDifference / 1000 / 60) % 60),
+      seconds: Math.floor((timeDifference / 1000) % 60)
+    });
+  }
+  
+  render() {
+
+    const { days, hours, minutes, seconds} = this.state;
+
+    return(
+      <div className="clock">
+        <div className="countdown-days"> {days} days</div>
+        <div className="countdown-hours"> {hours} hours</div>
+        <div className="countdown-minutes"> {minutes} minutes</div>
+        <div className="countdown-seconds"> {seconds} seconds</div>
+      </div>
+    )
+  }
+}
+
+export default Clock;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+// import {HolidayList} from '../../constants/Holidays';
+import Clock from './Clock/Clock';
 
 class Counter extends React.Component {
 
-  render() {    
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: '',
+      nextHoliday: {
+        id: '',
+        occasion: '',
+        date: '',
+        remarks: ''
+      }
+    }
+  }
+
+  render() {
+    const nextHolidayDate = Date.parse('August 15, 2019');
+    
     return ( 
-      <div>
-        <p>Days</p>
-        <p>Hours</p>
-        <p>Minutes</p>
-        <p>Seconds</p>
-      </div>
+        <div className="event-container">
+          <Clock nextHolidayDate={nextHolidayDate} />
+          {/* <p className="event-name">Days until '{this.state.nextHoliday.occasion}'</p> */}
+          {/* <p className="event-date">{this.state.nextHoliday.date}</p> */}
+        </div>
     );
   }
 }
 
-export default Counter;
+
+export default Counter;                         
